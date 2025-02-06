e0b125005843b2d02f56450bca7a330650d38552
Mirko Fenrich <fenrich@designpark.de>
2025-02-06 20:35:51 +0100

    Versuch1

--- src/WebappBlockBundle.php
+++ src/WebappBlockBundle.php
@@ -4,17 +4,65 @@ declare(strict_types=1);
 
 namespace Depa\WebappBlockBundle;
 
+use Nijens\ProtocolStream\Stream\Stream;
+use Nijens\ProtocolStream\StreamManager;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class WebappBlockBundle extends Extension
+class WebappBlockBundle extends AbstractBundle
 {
 
-    public function load(array $configs, ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.xml');
+        parent::build($container);
+
+        $container->addCompilerPass(new SettingsFormPass());
+    }
+    /*public function build(ContainerBuilder $container): void
+    {
+        $rootDirectory = $container->getParameter('kernel.project_dir');
+        $this->registerStream($rootDirectory);
+    }*/
+    public function boot(): void
+    {
+        $rootDirectory = $this->container->get('kernel')->getProjectDir();
+        $this->registerStream($rootDirectory);
+
+        parent::boot();
+    }
+
+    /**
+     * Register the stream for the given $rootDirectory.
+     *
+     * @param string $rootDirectory
+     */
+    private function registerStream($rootDirectory): void
+    {
+        $streamManager = new StreamManager();
+        if (is_null($streamManager->getStream('webapp-block-bundle'))) {
+            $stream = new Stream('webapp-block-bundle', [
+                'blocks' => __DIR__ . '/config/templates/blocks/',
+                'pages' => __DIR__ . '/config/templates/pages/',
+                /*'forms' => __DIR__ . '/Resources/forms/',
+                'properties' => __DIR__ . '/Resources/templates/properties/',
+                'app-properties' => $rootDirectory . '/config/templates/PixelSuluBlockBundle/properties/',
+                'app-property-params' => $rootDirectory . '/config/templates/PixelSuluBlockBundle/params/',
+    */        ]);
+
+            StreamManager::create()->registerStream($stream);
+        }
+    }
+
+    public function loadExtension(
+        array                                                                                                  $config,
+        ContainerConfigurator $container,
+        ContainerBuilder                                                                                       $builder,
+    ): void
+    {
+        $container->import('../config/services.yaml');
     }
 }
